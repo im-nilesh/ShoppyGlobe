a1a11f9b9d335ddefcf657a3aa16fcf78f0fad1b
im-nilesh <nileshdadab@gmail.com>
2026-06-05 13:10:16 +0530

    feat: implement product fetching and loading states in Home component

--- ShoppyGlobe/src/pages/Home.jsx
+++ ShoppyGlobe/src/pages/Home.jsx
@@ -1,4 +1,32 @@
+import { useEffect, useState } from "react";
+
 function Home() {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const respone = await fetch("https://dummyjson.com/products");
+        const data = await respone.json();
+        setProducts(data.products);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+  if (error) {
+    return <h1>{error}</h1>;
+  }
   return (
     <>
       <h1>Home Page</h1>
