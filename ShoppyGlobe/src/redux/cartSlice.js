15fe813e2758147506b325390eb3181720d6aa12
im-nilesh <nileshdadab@gmail.com>
2026-06-09 11:25:11 +0530

    feat: add cartSlice and store configuration for cart management

---
+++ ShoppyGlobe/src/redux/cartSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: [],
+  reducers: {
+    addToCart(state, action) {
+      state.push(action.payload);
+    },
+    removeFromCart(state, action) {
+      return state.filter((item) => {
+        return item.id !== action.payload;
+      });
+    },
+    clearCart() {
+      return [];
+    },
+  },
+});
+
+export const { addToCart, removeFromCart, clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
