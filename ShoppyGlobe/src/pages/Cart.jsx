9f19594188a620bd65b1828be9f0f735b2c53708
im-nilesh <nileshdadab@gmail.com>
2026-06-10 11:41:38 +0530

    feat: enhance Cart component with item removal and total calculation functionality

--- ShoppyGlobe/src/pages/Cart.jsx
+++ ShoppyGlobe/src/pages/Cart.jsx
@@ -1,12 +1,41 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
+import { removeFromCart, clearCart } from "../redux/cartSlice";
 function Cart() {
   const cart = useSelector((state) => state.cart);
   console.log(cart);
+  const dispatch = useDispatch();
+  const total = cart.reduce((acc, product) => {
+    return acc + product.price;
+  }, 0);
 
   return (
     <>
-      <h1>Cart Page</h1>
+      <h1>Cart</h1>
+
+      {cart.map((product) => {
+        return (
+          <div key={product.id}>
+            <h2>{product.title}</h2>
+            <h3>{product.price}</h3>
+            <button
+              onClick={() => {
+                dispatch(removeFromCart(product.id));
+              }}
+            >
+              Remove From Cart
+            </button>
+          </div>
+        );
+      })}
+      <h2>Total: {total}</h2>
+      <button
+        onClick={() => {
+          dispatch(clearCart());
+        }}
+      >
+        Clear Cart
+      </button>
     </>
   );
 }
