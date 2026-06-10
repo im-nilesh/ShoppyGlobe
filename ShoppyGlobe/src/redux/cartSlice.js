6f193af4fd024d7337e1143a47a58a4eef54471a
im-nilesh <nileshdadab@gmail.com>
2026-06-10 16:44:12 +0530

    feat: enhance cartSlice with quantity management and additional reducers

--- ShoppyGlobe/src/redux/cartSlice.js
+++ ShoppyGlobe/src/redux/cartSlice.js
@@ -5,19 +5,52 @@ const cartSlice = createSlice({
   initialState: [],
   reducers: {
     addToCart(state, action) {
-      console.log(action.payload);
-      state.push(action.payload);
+      const existingProduct = state.find(
+        (item) => item.id === action.payload.id,
+      );
+
+      if (existingProduct) {
+        existingProduct.quantity += 1;
+      } else {
+        state.push({
+          ...action.payload,
+          quantity: 1,
+        });
+      }
     },
+
     removeFromCart(state, action) {
-      return state.filter((item) => {
-        return item.id !== action.payload;
-      });
+      return state.filter((item) => item.id !== action.payload);
     },
+
     clearCart() {
       return [];
+    },
+
+    incrementQuantity(state, action) {
+      const product = state.find((item) => item.id === action.payload);
+
+      if (product) {
+        product.quantity += 1;
+      }
+    },
+
+    decrementQuantity(state, action) {
+      const product = state.find((item) => item.id === action.payload);
+
+      if (product && product.quantity > 1) {
+        product.quantity -= 1;
+      }
     },
   },
 });
 
-export const { addToCart, removeFromCart, clearCart } = cartSlice.actions;
+export const {
+  addToCart,
+  removeFromCart,
+  clearCart,
+  incrementQuantity,
+  decrementQuantity,
+} = cartSlice.actions;
+
 export default cartSlice.reducer;
